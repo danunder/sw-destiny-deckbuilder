e2302493907c58943bc8c29f8a3c8c46a9301a8f
Daniel Robertson <danieljrobertson@gmail.com>
2021-02-24 17:19:48 -0500

    wrestle with mui

--- src/components/SWCard.js
+++ src/components/SWCard.js
@@ -1,21 +1,39 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 
+import Image from 'material-ui-image'
+import { Box } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  
+  card: {
+    margin: '50px'
+  },
+  
+  cardImage: {
+    width: '320px',
+    height: 'auto',
+    
+    
+  }
+  
+  
+
+}))
 
 
 export default function SWCard(props) {
-  
+  const classes = useStyles()
   
   return (
-    <Card>
-      <CardMedia
-        style={{height: 400, width: 200, paddingTop: '56.25%'}}
-        image={props.img}
-        title={props.name}
-    />        
-    </Card>
+    
+      <Image
+      src={props.img}
+      imageStyle={{width:'320px', height: 'auto'}}
+    >
+      </Image>
+      
+    
   )
 }
